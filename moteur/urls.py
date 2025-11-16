829717fc2d7a729bf9214e6dabb0aa7bec35e73e
Hamza0u <hamza@DESKTOP-BEVG8KS.>
2025-11-16 21:43:21 +0100

    ajout du fichier index + Recherche avec indexé

--- moteur/urls.py
+++ moteur/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Page principale : formulaire + résultats
     path('', views.index, name='search_index'),
 ]
--- moteur/views.py
+++ moteur/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+import json
 import os
+from django.shortcuts import render
 from django.conf import settings
 
-
+# chemins
+INDEX_FILE = os.path.join(settings.BASE_DIR, "moteur","books" , "index.json")
 BOOKS_DIR = os.path.join(settings.BASE_DIR, "moteur", "books", "gutendex_books")
 
+# charger l'index
+with open(INDEX_FILE, "r", encoding="utf-8") as f:
+    INDEX = json.load(f)
+
+# recherche texte intégral
 def search_books(keyword):
     results = []
     keyword_lower = keyword.lower()
@@ -17,11 +24,29 @@ def search_books(keyword):
                     results.append(filename)
     return results
 
+# page principale
 def index(request):
-    results = []
-    keyword = ""
+    results_books = []
+    results_index = []
+    keyword_books = ""
+    keyword_index = ""
+
     if request.method == "POST":
-        keyword = request.POST.get("keyword", "").strip()
-        if keyword:
-            results = search_books(keyword)
-    return render(request, "searchapp/index.html", {"results": results, "keyword": keyword})
+        # recherche classique
+        keyword_books = request.POST.get("keyword", "").strip()
+        if keyword_books:
+            results_books = search_books(keyword_books)
+        
+        # recherche avec index
+        keyword_index = request.POST.get("mot", "").strip().lower()
+        if keyword_index in INDEX:
+            for book_id, count in INDEX[keyword_index].items():
+                results_index.append({"id": book_id, "count": count})
+            results_index = sorted(results_index, key=lambda x: x["count"], reverse=True)
+
+    return render(request, "searchapp/index.html", {
+        "results_books": results_books,
+        "results_index": results_index,
+        "keyword_books": keyword_books,
+        "keyword_index": keyword_index
+    })
